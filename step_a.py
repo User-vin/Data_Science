5b8e75e751d35ab52ab8357e425f2fee18499d16
scott barker <119344955+etudiantdescartes@users.noreply.github.com>
2023-04-20 20:36:41 +0200

    Update step_a.py

--- step_a.py
+++ step_a.py
@@ -2,14 +2,14 @@ import xgboost as xgb
 from google.colab import drive
 import numpy as np
 import pandas as pd
-from copy import deepcopy
-from sklearn.metrics import f1_score, accuracy_score, recall_score
+from sklearn.metrics import f1_score, recall_score, precision_score, multilabel_confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
 drive.mount('/content/gdrive', force_remount=True)
 
 
-
+#Function that takes 2d array and returns an ndarray with 7 columns (the features extracted from the data)
 def add_features(X):
 
   h, w = X.shape
@@ -51,33 +51,83 @@ def add_features(X):
   
   return new_array
 
-
+#Read CSVs
 test_data = pd.read_csv('/content/gdrive/MyDrive/InputTest.csv')
 train_data = pd.read_csv('/content/gdrive/MyDrive/InputTrain.csv')
 train_labels = pd.read_csv('/content/gdrive/MyDrive/StepOne_LabelTrain.csv')
 
+#Remove House_id and Index columns
 test_data = test_data.drop('House_id', axis=1).drop('Index', axis=1).astype(float)
 train_data = train_data.drop('House_id', axis=1).drop('Index', axis=1).astype(float)
 train_labels = train_labels.drop('House_id', axis=1).drop('Index', axis=1)
 
+#Convert CSVs to numpy
 X_train = train_data.to_numpy()
 X_test = test_data.to_numpy()
 y_train = train_labels.to_numpy()
 
-
+#Feature extraction
 X_train = add_features(X_train)
 X_test = add_features(X_test)
 
+#Split into train and test
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, shuffle=False)
 
-def training(X_train, X_test, y_train):
-  model = xgb.XGBClassifier()
-  model.fit(X_train, y_train)
-  y_pred = model.predict(X_test)
-  return y_pred
+#function to train and save results plots
+def training(X_train, X_test, y_train, y_test):
+    model = xgb.XGBClassifier(tree_method='gpu_hist', gpu_id=0)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    
+    #Metrics for each appliances
+    f1_scores = f1_score(y_test, y_pred, average=None)
+    recalls = recall_score(y_test, y_pred, average=None)
+    precisions = precision_score(y_test, y_pred, average=None)
 
-y_pred = training(X_train, X_test, y_train)
+    #Average metrics
+    print(f1_score(y_test, y_pred, average='macro'))
+    print(recall_score(y_test, y_pred, average='macro'))
+    print(precision_score(y_test, y_pred, average='macro'))
 
-cols = ['Washing Machine', 'Dishwasher', 'Tumble Dryer', 'Microwave', 'Kettle']
-df = pd.DataFrame(y_pred, columns=cols)
-df.index.name = 'Index'
-df.to_csv('res.csv', index=True)
+    print(f1_scores)
+    print(recalls)
+    print(precisions)
+
+    #Saving plots
+    classes = ['Dishwasher', 'Kettle', 'Microwave', 'Tumble Dry.', 'Washing mach.']
+
+    plt.bar(classes, f1_scores)
+    plt.xlabel('Class')
+    plt.ylabel('F1-score')
+    plt.title('F1-scores')
+    plt.savefig('f1_scores.png')
+    plt.close()
+
+    plt.bar(classes, recalls)
+    plt.xlabel('Class')
+    plt.ylabel('Recall')
+    plt.title('Recalls')
+    plt.savefig('recalls.png')
+    plt.close()
+
+    plt.bar(classes, precisions)
+    plt.xlabel('Class')
+    plt.ylabel('Precision')
+    plt.title('Precisions')
+    plt.savefig('precisions.png')
+    plt.close()
+
+    #Confusion matrix
+    ml_cm = multilabel_confusion_matrix(y_test, y_pred)
+    for i, cm in enumerate(ml_cm):
+        fig, ax = plt.subplots(figsize=(5, 5))
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', ax=ax)
+        ax.set_title(classes[i])
+        ax.set_xlabel('Predicted')
+        ax.set_ylabel('True')
+        plt.tight_layout()
+
+        plt.savefig(f'confusion_matrix_class_{i + 1}.png')
+        plt.close(fig)
+
+training(X_train, X_test, y_train, y_test)
